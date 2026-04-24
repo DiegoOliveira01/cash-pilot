0b4e7dd485bc805027593fba715d6f4fc746c9b3
DiegoOliveira01 <diegofontes.002@gmail.com>
2026-04-24 18:38:45 -0300

    feat: add swagger api documentation

--- src/main/java/com/diego/cashpilot/config/SecurityConfig.java
+++ src/main/java/com/diego/cashpilot/config/SecurityConfig.java
@@ -39,6 +39,11 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .authenticationProvider(authenticationProvider())
