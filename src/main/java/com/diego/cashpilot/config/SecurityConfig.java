f598f3b080eec0c511c163a78cfa6a2003da759f
DiegoOliveira01 <diegofontes.002@gmail.com>
2026-04-24 20:31:01 -0300

    feat: update SecurityConfig

--- src/main/java/com/diego/cashpilot/config/SecurityConfig.java
+++ src/main/java/com/diego/cashpilot/config/SecurityConfig.java
@@ -77,7 +77,10 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:4200",
+                "https://cashpilot-frontend.vercel.app"
+                ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
